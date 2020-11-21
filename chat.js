b19bfb9b39720c401cf715c240e32d723cbd89f2
tahaddyat <70242053+tahaddyat@users.noreply.github.com>
2020-11-21 15:39:15 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,184 @@
 $('.chatCovoBox').scrollTop($('.chatCovoBox')[0].scrollHeight);
 
+$(document).ready(function(){$('.chatCovoBox').scrollTop($('.chatCovoBox')[0].scrollHeight);
+
 $(document).ready(function(){
+    resizeDiv();
+});
+
+window.onresize = function(event) {
+    resizeDiv();
+}
+
+function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $("#chatBox").css({"height": vph + "px"});
+}
+
+// firebase.database().ref().child("Chat").on("child_added", function (snapshot) {
+// if(snapshot.key!=(firebase.auth().currentUser.uid))
+// {
+//
+//  var snapData =snapshot.val();
+//  var otherMessages = snapData.Messages.chatMessage;
+//
+//  var innerHtml = document.querySelector(".chatCovoBox").innerHTML;
+//  innerHtml += "<div class=\"chatRow mb-2 text-left\"><div class=\"container myChatText text-right\"><span class=\"myName text-center\">"+(snapshot.key+"").slice(0, 5)+"</span><br><span class=\"myText d-flex self-align-end\">"+otherMessages+"</span></div></div>";
+//    document.querySelector(".chatCovoBox").innerHTML = innerHtml;
+//
+//    console.log(snapData.Messages.chatMessage);
+//
+// }
+// });
+firebase.database().ref().child("Chat").on("child_changed", function(snapshot) {
+
+  var changedPost = snapshot.val();
+
+  if(snapshot.key==(firebase.auth().currentUser.uid))
+  {
+    // var myMessages = changedPost.Messages.chatMessage;
+    // var innerHtml = document.querySelector(".chatCovoBox").innerHTML;
+    // innerHtml += "<div class=\"chatRow mb-2 text-left\"><div class=\"container myChatText text-right\"><span class=\"myName text-center\">زائر</span><br><span class=\"myText d-flex self-align-end\">"+myMessages+"</span></div></div>";
+    //   document.querySelector(".chatCovoBox").innerHTML = innerHtml;
+    // console.log(snapshot.key+" : "+myMessages);
+  }
+  else
+  {
+    var othersMessages = changedPost.Messages.chatMessage;
+    
+    var audio = document.getElementById("audio");
+    audio.play();
+
+    var innerHtml = document.querySelector(".chatCovoBox").innerHTML;
+    innerHtml += "<div class=\"chatRow mb-2 text-left\"><div class=\"container othersChatText text-right\"><span class=\"myName text-center\">"+(snapshot.key+"").slice(0, 5)+"</span><br><span class=\"myText d-flex self-align-end\">"+othersMessages+"</span></div></div>";
+      document.querySelector(".chatCovoBox").innerHTML = innerHtml;
+    console.log(snapshot.key+" : "+othersMessages);
+    //START - To Keep the Scroll Pinned Down //
+    var messageBody = document.querySelector('.chatCovoBox');
+    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+    //END - To Keep the Scroll Pinned Down //
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+});
+
+document.querySelector("#chatInputMessage").addEventListener("focus", function(){
+  $('.chatBox').scrollTop($('.chatBox')[0].scrollHeight);
+});
+
+document.querySelector("#chatSendButton").addEventListener("click", function()
+{
+  const chatMessage = document.querySelector("#chatInputMessage").value;
+  document.querySelector("#chatInputMessage").value ="";
+
+  var chatRef = firebase.database().ref().child("Chat");
+  var userID = firebase.auth().currentUser.uid;
+  // var otherID = firebase.auth().
+  var userRef = chatRef.child(userID);
+  var myMessages = userRef.child("Messages");
+
+  if(chatMessage!="")
+  {
+    var innerHtml = document.querySelector(".chatCovoBox").innerHTML;
+    var audio = document.getElementById("audio");
+    audio.play();
+
+    innerHtml += "<div class=\"chatRow mb-2 text-right\"><div class=\"container myChatText text-right\"><span class=\"myName text-center\">أنا</span><br><span class=\"myText d-flex self-align-end\">"+chatMessage+"</span></div></div>";
+      document.querySelector(".chatCovoBox").innerHTML = innerHtml;
+        var userData =
+        {
+          "chatMessage": chatMessage,
+        };
+        myMessages.set(userData, function(error){
+         if(error)
+          {
+           alert("Error!");
+          }
+         else
+          {
+          }
+      });
+      // listen for incoming messages
+      myMessages.on("child_added", function (snapshot) {
+
+      var snapData =snapshot.val();
+      // console.log(snapData);
+   });
+   //START - To Keep the Scroll Pinned Down //
+   var messageBody = document.querySelector('.chatCovoBox');
+   messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+   //END - To Keep the Scroll Pinned Down //
+  }
+
+});
+
+document.querySelector("#chatInputMessage").addEventListener("keypress", function (e) {
+    if (e.key === "Enter")
+    {
+
+      const chatMessage = document.querySelector("#chatInputMessage").value;
+      document.querySelector("#chatInputMessage").value ="";
+
+      var chatRef = firebase.database().ref().child("Chat");
+      var userID = firebase.auth().currentUser.uid;
+      // var otherID = firebase.auth().
+      var userRef = chatRef.child(userID);
+      var myMessages = userRef.child("Messages");
+
+      if(chatMessage!="")
+      {
+        var audio = document.getElementById("audio");
+        audio.play();
+
+        var innerHtml = document.querySelector(".chatCovoBox").innerHTML;
+        innerHtml += "<div class=\"chatRow mb-2 text-right\"><div class=\"container myChatText text-right\"><span class=\"myName text-center\">أنا</span><br><span class=\"myText d-flex self-align-end\">"+chatMessage+"</span></div></div>";
+          document.querySelector(".chatCovoBox").innerHTML = innerHtml;
+            var userData =
+            {
+              "chatMessage": chatMessage,
+            };
+            myMessages.set(userData, function(error){
+             if(error)
+              {
+               alert("Error!");
+              }
+             else
+              {
+              }
+          });
+          // listen for incoming messages
+          myMessages.on("child_added", function (snapshot) {
+
+          var snapData =snapshot.val();
+          // console.log(snapData);
+       });
+       //START - To Keep the Scroll Pinned Down //
+       var messageBody = document.querySelector('.chatCovoBox');
+       messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+       //END - To Keep the Scroll Pinned Down //
+       $('.chatBox').scrollTop($('.chatBox')[0].scrollHeight);
+      }
+    }
+});
+ // Disable form submit on "Enter" key pressing
+$(document).on("keypress", 'form', function (e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+        e.preventDefault();
+        return false;
+    }
+});
+// To prevent the delay in database retrieval from disabling the scroll pinned down. So, the below only execute last
+window.onload = function() {
+  //START - To Keep the Scroll Pinned Down //
+  var messageBody = document.querySelector('.chatCovoBox');
+  messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+  //END - To Keep the Scroll Pinned Down //
+  $('.chatBox').scrollTop($('.chatBox')[0].scrollHeight);
+};
+
     resizeDiv();
 });
 
